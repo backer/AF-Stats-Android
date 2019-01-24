49c012e734a1216061de9d18597b18c7da1f64af
Brian Acker <backer@brightcove.com>
2019-01-23 20:51:01 -0500

    Show splash for 3 seconds and then transition to new home screen

--- app/src/main/java/acker/brian/af_stats/SplashActivity.kt
+++ app/src/main/java/acker/brian/af_stats/SplashActivity.kt
@@ -1,6 +1,8 @@
 package acker.brian.af_stats
 
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
 import android.support.v7.app.AppCompatActivity
 
 class SplashActivity : AppCompatActivity() {
@@ -13,5 +15,9 @@ class SplashActivity : AppCompatActivity() {
         super.onStart()
         // todo make any necessary network calls/configuration here
         // until then, just show splash for 3 seconds and then transition to MainActivity
+        val runnable = Runnable {
+            startActivity(Intent(this@SplashActivity, MainActivity::class.java))
+        }
+        Handler().postDelayed(runnable, 3000)
     }
 }
